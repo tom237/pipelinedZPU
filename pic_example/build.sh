22f7246e05605a40405724a993df28f51b7d3c06
Tomi <tom23781@gmail.com>
2014-07-03 20:19:20 +0200

    first commit

--- pic_example/build.sh
+++ pic_example/build.sh
@@ -2,4 +2,4 @@ export PATH=$PATH:/home/tomi/zylin_cpu/new/bin
 zpu-elf-gcc -O3 -phi `pwd`/int.c -o int.elf -Wl,--relax -Wl,--gc-sections  -g
 zpu-elf-objdump --disassemble-all >int.dis int.elf
 zpu-elf-objcopy -O binary int.elf int.bin
-zpuromcoegen int.bin int.coe
+zpuromgen int.bin rom.v 32768
